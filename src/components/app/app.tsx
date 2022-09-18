becc06fa8fb1f21bde76f28077c69ab37fa6f3cf
DenisPepper <thech@yandex.ru>
2022-09-18 23:56:45 +0300

    add async loading for components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import '../../index.scss';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import {AppPath} from "../../settings";
-import MainPage from "../../pages/main-page/main-page";
-import AboutPage from "../../pages/about-page/about-page";
 import Page404 from "../../pages/404-page/404-page";
+import {MainPageAsync} from "../../pages/main-page/main-page-async";
+import {AboutPageAsync} from "../../pages/about-page/about-page-async";
+import {Suspense} from "react";
+
 
 export const App = (): JSX.Element => {
 
@@ -12,13 +14,15 @@ export const App = (): JSX.Element => {
             <div className='app'>
                <Link to={AppPath.MainPage}> MAIN PAGE </Link>
                <Link to={AppPath.AboutPage}> ABOUT PAGE </Link>
-                <Routes>
-                    <Route path={AppPath.MainPage}>
-                        <Route index element = {<MainPage />} />
-                        <Route path = {AppPath.AboutPage} element = {<AboutPage />} />
-                        <Route path = {AppPath.Page404} element={<Page404 />}/>
-                    </Route>
-                </Routes>
+                <Suspense fallback={<div>Loading ...</div>}>
+                    <Routes>
+                        <Route path={AppPath.MainPage}>
+                            <Route index element = {<MainPageAsync />} />
+                            <Route path = {AppPath.AboutPage} element = {<AboutPageAsync />} />
+                            <Route path = {AppPath.Page404} element={<Page404 />}/>
+                        </Route>
+                    </Routes>
+                </Suspense>
            </div>
         </BrowserRouter>
     );
