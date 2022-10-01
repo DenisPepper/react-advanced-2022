523a52e6c68a903b4627c6a85bfbe9aa439188d6
DenisPepper <thech@yandex.ru>
2022-10-01 17:33:28 +0300

    minor fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { render } from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
-import App  from "app/app";
-import { ThemeProvider } from "app/providers/theme-provider";
-import "./shared/config/i18n-config";
+import { BrowserRouter } from 'react-router-dom';
+import App from 'app/app';
+import { ThemeProvider } from 'app/providers/theme-provider';
+import './shared/config/i18n-config';
 
 const root = document.getElementById('root') as HTMLElement;
 
@@ -12,5 +12,5 @@ render(
             <App />
         </ThemeProvider>
     </BrowserRouter>,
-    root
+    root,
 );
