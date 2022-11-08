64854489229ea4376cbb16560931f0f338a17ee5
DenisPepper <thech@yandex.ru>
2022-11-08 20:22:28 +0300

    fix theme-provider.tsx

--- src/app/providers/theme-provider/ui/theme-provider.tsx
+++ src/app/providers/theme-provider/ui/theme-provider.tsx
@@ -1,14 +1,25 @@
-import { useMemo, useState } from 'react';
+import { ReactNode, useMemo, useState } from 'react';
 import { AppTheme, DEFAULT_APP_THEME, LOCAL_STORAGE_THEME_KEY } from 'shared/settings';
 import { ThemeContext } from '../lib/theme-context';
 
 type ThemeProviderProps = {
-    children: JSX.Element;
+    children: ReactNode;
+};
+
+const getTheme = (): AppTheme => {
+    switch (localStorage.getItem(LOCAL_STORAGE_THEME_KEY)) {
+    case AppTheme.Light:
+        return AppTheme.Light;
+    case AppTheme.Dark:
+        return AppTheme.Dark;
+    default:
+        return DEFAULT_APP_THEME;
+    }
 };
 
 export function ThemeProvider(props : ThemeProviderProps): JSX.Element {
     const { children } = props;
-    const currentTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as AppTheme || DEFAULT_APP_THEME;
+    const currentTheme = getTheme();
     const [theme, setTheme] = useState<AppTheme>(currentTheme);
     const value = useMemo(() => ({ theme, setTheme }), [theme]);
     return (
