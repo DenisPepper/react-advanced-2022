ceb071d08df67489b77784b6a7d610e947d1c974
DenisPepper <thech@yandex.ru>
2022-12-01 15:24:27 +0300

    modify AppButton

--- src/shared/ui/app-button/app-button.tsx
+++ src/shared/ui/app-button/app-button.tsx
@@ -18,22 +18,26 @@ type AppButtonProps = {
     theme?: AppButtonThemes;
     square?: boolean;
     size?: AppButtonSizes;
+    disabled?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
 export function AppButton(props: AppButtonProps): JSX.Element {
     const {
-        className, theme, square, size, children, ...restProps
+        className, theme, square, size, disabled = false, children, ...restProps
     } = props;
 
-    const classNames = buildNames([
-        cls[theme],
-        cls[size],
-        square ? cls.square : '',
-        cls.app_button,
-        className]);
+    const classNames = buildNames(
+        [cls[theme], cls[size], cls.app_button, className],
+        { [cls.square]: square, [cls.disabled]: disabled },
+    );
 
     return (
-        <button type="button" className={classNames} {...restProps}>
+        <button
+            type="button"
+            className={classNames}
+            disabled={disabled}
+            {...restProps}
+        >
             {children}
         </button>
     );
