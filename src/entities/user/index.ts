a8280d82793782a7dc23eab7ea622527ee0d2bff
DenisPepper <thech@yandex.ru>
2022-12-01 15:26:58 +0300

    modify User model

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -3,3 +3,5 @@ export { userReducer, userActions } from './model/slice/user-slice';
 export { User } from './ui/user';
 
 export { UserSchema, UserType } from './model/types/user-schema';
+
+export { getUserAuthData } from './model/selectors/get-user-auth-data/get-user-auth-data';
--- src/entities/user/model/slice/user-slice.ts
+++ src/entities/user/model/slice/user-slice.ts
@@ -1,14 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { UserSchema } from 'entities/user/model/types/user-schema';
-
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { UserSchema, UserType } from 'entities/user/model/types/user-schema';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+/* eslint-disable no-param-reassign */
 const initialState: UserSchema = {
-    authData: undefined,
+    authData: null,
 };
 
 export const UserSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {},
+    reducers: {
+        setAuthData: (state, action: PayloadAction<UserType>) => {
+            state.authData = action.payload;
+        },
+
+        setAuthDataFromLocalStorage: (state) => {
+            const authData = JSON.parse(localStorage.getItem(USER_LOCALSTORAGE_KEY));
+            if (authData) {
+                state.authData = authData;
+            }
+        },
+
+        logOut: (state) => {
+            localStorage.removeItem(USER_LOCALSTORAGE_KEY);
+            state.authData = null;
+        },
+    },
 });
 
 export const { actions: userActions } = UserSlice;
